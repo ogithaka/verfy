864cd99e6d0857988b6207b9b9a31a0ff6fa459f
ogithaka <ogithaka@gmail.com>
2024-10-27 11:59:39 +0300

    feat: enhance menu hover effect and update company name
    a. Added `:hover` style to the `.trigger_menu` class, providing visual feedback for menu items to improve usability and indicate interactivity.
    b. Updated the footer copyright notice from "Guardy" to "Verfy," reflecting the correct company name and aligning with recent rebranding.
    c. Retained the informational comment about the `top` value in the layout, offering context for future development adjustments.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Styles } from '@components';
 export default function Footer() {
     return (
         <footer className={Styles.footer}>
-            <p>© {new Date().getFullYear()} Guardy. All rights reserved.</p>
+            <p>© {new Date().getFullYear()} Verfy. All rights reserved.</p>
         </footer>
     );
 }
